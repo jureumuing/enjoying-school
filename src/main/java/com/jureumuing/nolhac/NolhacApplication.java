bd9760a392bc412a047bb204004cb19f642e8140
dldmldlsy <dldmldlsy@naver.com>
2022-12-18 21:47:16 +0900

    프로젝트 생성

---
+++ src/main/java/com/jureumuing/nolhac/NolhacApplication.java
@@ -0,0 +1,13 @@
+package com.jureumuing.nolhac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolhacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolhacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jureumuing/nolhac/NolhacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jureumuing.nolhac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolhacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
