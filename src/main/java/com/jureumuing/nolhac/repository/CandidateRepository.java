bc02d692cd3b4154ef7f64d98d786279ef1df3c7
prkim99@gmail.com <prkim99@gmail.com>
2022-12-19 02:26:27 +0900

    레포지토리 작성

---
+++ src/main/java/com/jureumuing/nolhac/repository/CandidateRepository.java
@@ -0,0 +1,21 @@
+package com.jureumuing.nolhac.repository;
+
+import com.jureumuing.nolhac.controller.CandidateController;
+import com.jureumuing.nolhac.entity.CandidateEntity;
+import com.jureumuing.nolhac.entity.UserEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@Mapper
+public interface CandidateRepository {
+
+    int insert(CandidateEntity candidateEntity);
+    int delete(int candidateId);
+    ArrayList<CandidateEntity> selectAll();
+    int update(int candidateId, int admitCount);
+
+}
